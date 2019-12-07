997af5ae08656e9110318682de94f1d1588efaf8
Lee Opperman <leeopperman@Lees-MacBook-Pro.local>
2019-12-07 21:47:28 +0200

    Login page changes

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ const clientSchema = new mongoose.Schema({
 });
 
 // Create a model for schema
-
+const queryDB = mongoose.model('clients', clientSchema);
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/login.html');
 });
@@ -27,8 +27,17 @@ router.get('/register', function(req, res, next) {
   res.sendFile(__dirname + '/register.html');
 });
 
-router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/style.css');
+router.post('/api/login', function(req, res) {
+  console.log("login request received: ", req.body);
+  queryDB.find({email: req.body.email, password: req.body.password})
+  .then(data => {
+    console.log("query data: ", JSON.stringify(data));
+    console.log("data.email: ", data[0].email);
+    if (req.body.email === data[0].email && req.body.password === data[0].password )
+      res.send(JSON.stringify(data[0]))
+    else
+      return null; 
+    });
 });
 
 router.post('/api/register', function(req, res){
