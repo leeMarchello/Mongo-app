1521bf4f17fe419c52c7680698ffc07defd74f0a
jinww <mac@Jinweinono.local>
2019-12-10 18:46:14 +0200

    table display data

--- public/js/admin.js
+++ public/js/admin.js
@@ -10,6 +10,23 @@ fetch('http://localhost:9000/api/get/students')
 .then( jsonData => {
     console.log("final data: ", jsonData)
     listOfStudents = jsonData;
+    var listlen = listOfStudents.length;
+    var mytable = document.getElementById('table01');
+    for (var i = 0; i < listlen; i++) {
+        var row = document.createElement('tr');
+        row.id= "row"+i;
+        document.getElementById("table01").appendChild(row);
+        var valuelist = Object.values(listOfStudents[i]);
+        var objlen = Object.keys(listOfStudents).length;
+    }
+    console.log(objlen);
+        for(var x =0; x< objlen; x++){
+            console.log("yeah");
+            var cell = document.createElement("td");
+            var cellbody = document.createTextNode(valuelist[x]);
+            cell.appendChild(cellbody);
+            document.getElementById("table01").appendChild(cell)  
+        }
 })
  .catch( error => console.log("Error during fetch: ",error));
 
