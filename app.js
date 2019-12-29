8e537814037657fef235e78e3a446acf21ae42fb
Lee Opperman <lee.opperman5@gmail.com>
2019-12-29 23:07:37 +0200

    authentication implementation

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
-var cookieParser = require('cookie-parser');
-var createError = require('http-errors');
-var mongoose = require('mongoose');
-var express = require('express');
-var logger = require('morgan');
-var path = require('path');
-// jin wei made this
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const flash = require('connect-flash');
+const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+
+//app.use(express.static('public'));
+
+//require('./config/passport');
+
 // Connect to DB
 const db_name = 'school';
 mongoose.connect(`mongodb://localhost/${db_name}`)
   .then(() => console.log(`Connected to MongoDB database ${db_name}`))
   .catch(err => console.error("Could not connect to MongoDB...", err));
 
-// View page handlers
-var indexRouter = require('./routes/index');
-var app = express();
+// 1. Setup express app
+const app = express();
+app.use(morgan('dev'));
 
-// view engine setup
+// 2. View page handlers
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', expressHandlebars({defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
 
-// serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// 3. Session setup 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  cookie: { maxAge: 60000},
+  secret: 'codeworkrsecret',
+  saveUninitialized: false,
+  resave: false,
+}));
 
-// API endpoint handlers 
-app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// 4. Flash 
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success')
+  res.locals.error_messages = req.flash('error')
+  next()
+});
+
+// 5. Page handlers 
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+// 6. catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -42,10 +69,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('notFound');
 });
 
 module.exports = app;
